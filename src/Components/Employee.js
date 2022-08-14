f24b3fb1bcec52e7f12189954e24def8ffdade33
Mehmet Yildiz <yildiz655@gmail.com>
2022-08-14 23:38:07 +0300

    Web[1] : Axios system added, first try to send image

--- src/Components/Employee.js
+++ src/Components/Employee.js
@@ -3,15 +3,27 @@ import React, { useState, useEffect } from 'react'
 const defaultImageSrc = '/img/user.png'
 
 const initialfieldvalues = {
-    employeeId: 0,
-    employeeName: "",
-    occupation: '',
-    imageName: '',
-    imageSrc: defaultImageSrc,
-    imageFile: null
+    id: 0,
+    name: "",
+    filefolder: '',
+    filetype: '',
+    downloadedcount: 0,
+    lastdownloadeduser: '',
+    lastdownloadedip: '',
+    filepath: defaultImageSrc,
+    imageFile: null,
+    concurrencyStamp: '',
+    createdUser: '',
+    updatedUser: '',
+    deleteUser: '',
+    createTime: null,
+    updateTime: null,
+    deleteTime: null,
+    isActive: false
 }
 
-export default function Employee() {
+export default function Employee(props) {
+    const { addOrEdit } = props
     const [values, setValues] = useState(initialfieldvalues)
     const [errors, setErrors] = useState({})
     const handleInputChange = (e) => {
@@ -26,7 +38,7 @@ export default function Employee() {
                 setValues({
                     ...values,
                     imageFile: imageFile,
-                    imageSrc: x.target.result
+                    filepath: x.target.result
                 })
             }
             reader.readAsDataURL(imageFile)
@@ -34,22 +46,64 @@ export default function Employee() {
             setValues({
                 ...values,
                 imageFile: null,
-                imageSrc: defaultImageSrc
+                filepath: defaultImageSrc
             })
         }
     }
     const validate = () => {
         let temp = {}
-        temp.employeeName = values.employeeName == "" ? false : true
-        temp.imgSrc = values.imageSrc == defaultImageSrc ? false : true
+        temp.name = values.name == "" ? false : true
+        temp.filepath = values.filepath == defaultImageSrc ? false : true
         setErrors(temp)
         return Object.values(temp).every(x => x == true)
+    }
+
+    const resetForm = () => {
+        setValues(initialfieldvalues)
     }
 
     const handleSubmit = e => {
         e.preventDefault()
         if (validate()) {
-
+              const formData = new FormData();
+           // const formData = {
+                 //   formData.append('id', values.id)
+                //    formData.append('name', values.name)
+                 //   formData.append('filefolder', values.filefolder)
+                 //   formData.append('filetype', values.filetype)
+                 //   formData.append('downloadedcount', values.downloadedcount)
+                 //   formData.append('lastdownloadeduser', values.lastdownloadeduser)
+                 //   formData.append('lastdownloadedip', values.lastdownloadedip)
+                 //   formData.append('filepath', values.filepath)
+                    formData.append('imageFile', values.imageFile)
+                //    formData.append('concurrencyStamp', values.concurrencyStamp)
+                //    formData.append('createdUser', values.createdUser)
+                //    formData.append('updatedUser', values.updatedUser)
+               //     formData.append('deleteUser', values.deleteUser)
+                //    formData.append('createTime', values.createTime)
+                //    formData.append('updateTime', values.updateTime)
+                //    formData.append('deleteTime', values.deleteTime)
+                //    formData.append('isActive', values.isActive)
+              /*   'id': values.id,
+                'name': values.name,
+                'filefolder': values.filefolder,
+                'filetype': values.filetype,
+                'downloadedcount': values.downloadedcount,
+                'lastdownloadeduser': values.lastdownloadeduser,
+                'lastdownloadedip': values.lastdownloadedip,
+                'filepath': values.filepath,
+                'imageFile': values.imageFile,
+                'concurrencyStamp': values.concurrencyStamp,
+                'createdUser': values.createdUser,
+                'updatedUser': values.updatedUser,
+                'deleteUser': values.deleteUser,
+                'createTime': values.createTime,
+                'updateTime': values.updateTime,
+                'deleteTime': values.deleteTime,
+                'isActive': values.isActive */
+           // }
+            console.log('formDataback: ', formData);
+            addOrEdit(formData, resetForm)
         }
     }
     const applyerrorclass = field => ((field in errors && errors[field] == false) ? ' invalid-field' : '')
@@ -61,21 +115,15 @@ export default function Employee() {
             <form onSubmit={handleSubmit} autoComplete='off' noValidate>
                 <div className='card'>
                     <div className='card-body'>
-                        <img style={{ margin: '10px', width: '200px', height: '200px' }} src={values.imageSrc} className="card-img-top" />
+                        <img style={{ margin: '10px', width: '200px', height: '200px' }} src={values.filepath} className="card-img-top" />
                         <div className='form-group'>
-                            <input className={"form-control-file" + applyerrorclass('imgSrc')} accept='image/*' type="file"
+                            <input className={"form-control-file" + applyerrorclass('filepath')} accept='image/*' type="file"
                                 onChange={showPreview}
                             />
                         </div>
                         <div className='form-group'>
-                            <input className={"form-control" + applyerrorclass('employeeName')} placeholder='Employee Name' name='employeeName'
-                                value={values.employeeName}
-                                onChange={handleInputChange}
-                            />
-                        </div>
-                        <div className='form-group'>
-                            <input className='form-control' placeholder='Occupation' name='occupation'
-                                value={values.occupation}
+                            <input className={"form-control" + applyerrorclass('name')} placeholder=' Name' name='name'
+                                value={values.name}
                                 onChange={handleInputChange}
                             />
                         </div>
--- src/Components/EmployeeList.js
+++ src/Components/EmployeeList.js
@@ -1,18 +1,49 @@
+import axios from 'axios'
 import React from 'react'
 import Employee from './Employee'
 
 export default function EmployeeList() {
+    const employeeAPI = (url = "http://localhost:34891/api/File/Add") => {
+        return {
+            fetchAll: () => axios.get(url),
+            create: newRecord => axios.post(url, newRecord),
+            update: (id, updatedRecord) => axios.put(url + id, updatedRecord),
+            delete: id => axios.delete(url + id)
+        }
+    }
+    const addOrEdit = (formData, onSuccess) => {
+        employeeAPI().create(formData).then(response => {
+            console.log('response: ', response);
+            console.log("basarılı")
+            onSuccess();
+        }) .catch(error => {
+            console.log('erroraxios: ', error);
+        })
+       /*  axios({
+            method: 'post',
+            url: `http://localhost:34891/api/File/Add`,
+            data: formData
+        })
+            .then(response => {
+                console.log('response: ', response);
+                console.log("basarılı")
+                onSuccess();
+            })
+            .catch(error => {
+                console.log('erroraxios: ', error);
+            }) */
+    }
     return (
         <div className="row">
             <div className='col-md-12'>
-                <div class="jumbotron jumbotron-fluid py-4">
-                    <div class="container text-center">
-                        <h1 class="display-4">Employee Register</h1>
+                <div className="jumbotron jumbotron-fluid py-4">
+                    <div className="container text-center">
+                        <h1 className="display-4">Employee Register</h1>
                     </div>
                 </div>
             </div>
             <div className="col-md-4">
-                <Employee />
+                <Employee addOrEdit={addOrEdit} />
             </div>
             <div className="col-md-8">
                 <div>List of Employee</div>
